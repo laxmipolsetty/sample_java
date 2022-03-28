05d76575ae0ca6592cd97e59e999c7a378c932cc
laxmipolsetty <laxmipolsetty@gmail.com>
2022-03-28 12:07:06 +0000

    added multifle print files

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,17 @@ class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello world");
+	System.out.println("Hello world");
+	System.out.println("Hello world");
+	System.out.println("Hello world");
+	System.out.println("Hello world");
+	System.out.println("Hello world");
+	System.out.println("Hello world");
+	System.out.println("Hello world");
+	System.out.println("Hello world");
+	System.out.println("Hello world");
+	System.out.println("Hello world");
+	System.out.println("Hello world");
 
 	
 
