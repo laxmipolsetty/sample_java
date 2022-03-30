64e5d473bfe65358d5e314c0643577ba7636917f
laxmipolsetty <laxmipolsetty@gmail.com>
2022-03-30 09:55:18 +0000

    added jenkins line

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Today is Wednesday");
 	System.out.println("Welcome to Jenkins");
-	System.out.println("Jenkins start today")
+	System.out.println("Jenkins start today");
 	
 
     }
